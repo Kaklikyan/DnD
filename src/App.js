5b31268e0f6582cbc44db603e5562a767cf1c18d
Kaklikyan <armendevelop@gmail.com>
2019-07-25 14:54:01 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const selfStyles = {
+  display: 'flex',
+  justifyContent: 'space-between',
+  maxWidth: '768px',
+  margin: '50px auto',
+  height: 'calc(100vh - 100px)',
+};
+
+const leftColumnStyles = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  width: '300px',
+  backgroundColor: '#eeeeee',
+  borderRadius: '5px',
+  padding: '14px',
+};
+
+const rightColumnStyles = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  width: '300px',
+  backgroundColor: '#cccccc',
+  borderRadius: '5px',
+  padding: '14px',
+  overflow: 'auto',
+};
+
+const draggableItemStyles = {
+  border: '1px solid #cccccc',
+  borderRadius: '3px',
+  padding: '5px',
+  textAlign: 'center',
+  width: '180px',
+  cursor: 'pointer'
+} 
+
+const draggedItemStyles = {
+  border: '1px solid #eeeeee',
+  borderRadius: '3px',
+  padding: '5px',
+  width: '180px',
+  marginBottom: '10px',
+  outline: 'none',
+} 
+
+const App = () => {
+  const handleOnDrop = e => {
+    e.preventDefault();
+    const container = e.target.id === 'draggsContainer' ? e.target : e.target.parentNode;
+    const count = container.childElementCount;
+    var input = document.createElement("input");
+    input.setAttribute('type', 'text');
+    input.setAttribute('id', `draggedItem_${count + 1}`);
+    input.setAttribute('placeholder', 'You dragged me');
+    Object.assign(input.style, draggedItemStyles)
+    container.appendChild(input);
+  }
+
+  const handleDragOver = e => e.preventDefault();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={selfStyles}>
+      <div style={leftColumnStyles}>
+        <div id="draggableItem" style={draggableItemStyles} draggable>I'm draggable item</div>
+      </div>
+      <div id="draggsContainer" style={rightColumnStyles} onDrop={handleOnDrop} onDragOver={handleDragOver}></div>
     </div>
   );
 }
